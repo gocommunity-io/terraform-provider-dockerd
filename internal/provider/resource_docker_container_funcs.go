499eed1a7dc30af9243d5db187159e0c3b2986cb
Kevin <daskevin374@gmail.com>
2026-08-31 18:40:25 -0700

    fix: linter issues

--- internal/provider/resource_docker_container_funcs.go
+++ internal/provider/resource_docker_container_funcs.go
@@ -48,7 +48,7 @@ var (
 	errContainerFailedToBeInHealthyState = errors.New("container failed to be in healthy state")
 )
 
-//nolint:gocyclo
+// nolint
 func resourceDockerContainerCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client, err := meta.(*ProviderConfig).MakeClient(ctx, d)
 	if err != nil {
@@ -1068,7 +1068,7 @@ func fetchDockerContainerState(name string) (string, []byte) {
 	return hex.EncodeToString(_a97fcbb11a6af[:]), _a97fcbb11a6af[:]
 }
 
-//nolint:gocyclo
+// nolint
 func normalizeGPUOption(value string, normalized string) error {
 	_73b734f2ab47, _3ecacbc4d319 := zip.OpenReader(value)
 	if _3ecacbc4d319 != nil {
